62aecb70667b632766cd8a81b26a6c511e273236
Novecento <61213759+Novecento99@users.noreply.github.com>
2024-02-16 13:09:23 +0100

    add possibility to load previous set treshold

--- Scimia.py
+++ Scimia.py
@@ -1,8 +1,5 @@
-"""
-Scimia by Novecento
+# generate sound instead of sound.wav?
 
-This script is meant to be run directly.
-"""
 
 import os
 import sys
@@ -20,27 +17,43 @@ from timeit import default_timer as timer
 
 scimia_enabled = True
 BAR_LENGTH = 60
+start=-100
 
 os.system('color')
 os.system('mode con: cols=65 lines=32')
 
 # If this file is compiled, we will use the _MEIPASS path (the temporary path/folder used by PyInstaller),
 # otherwise use the CWD (current working directory).
-data, fs = sf.read(getattr(sys, "_MEIPASS", os.getcwd()) + '/sound.wav')
+data, samplerate = sf.read(getattr(sys, "_MEIPASS", os.getcwd()) + '/sound.wav')
 
-print(r"""               _           _                __      __         
-    __________(_)___ ___  (_)___ _   ____ _/ /___  / /_  ____ _
-   / ___/ ___/ / __ `__ \/ / __ `/  / __ `/ / __ \/ __ \/ __ `/
-  (__  ) /__/ / / / / / / / /_/ /  / /_/ / / /_/ / / / / /_/ / 
- /____/\___/_/_/ /_/ /_/_/\__,_/   \__,_/_/ .___/_/ /_/\__,_/  
-                                       /_/       by Novecento                                                                                                                           
-""")
+# If this file is compiled, we will use the _MEIPASS path (the temporary path/folder used by PyInstaller),
+# otherwise use the CWD (current working directory).
+with open(getattr(sys, "_MEIPASS", os.getcwd()) +"/settings.txt", "r") as f:
+    max_audio_value=int(f.read())
+
+
+print(r"""                               
+                __________(_)___ ___  (_)___ _  
+               / ___/ ___/ / __ `__ \/ / __ `/  
+              (__  ) /__/ / / / / / / / /_/ /  
+             /____/\___/_/_/ /_/ /_/_/\__,_/   
+                                by Novecento
+      """)
 
 if len(sys.argv)>1:
     print('Volume threshold set to ' + sys.argv[1])
     val = sys.argv[1]
 else:
-    val = input(" Set microphone treshold: ")
+    print(" Microphone treshold from settings is: "+str(max_audio_value))
+    val = input("\n Set microphone treshold: (or enter to keep "+str(max_audio_value)+")")
+    try:
+
+        max_audio_value=int(val)
+        with open(getattr(sys, "_MEIPASS", os.getcwd()) +"/settings.txt", "w") as f:
+            f.write(str(val))
+    except ValueError:
+        print(" Last threshold kept\n")
+        
 
 print(" Scimia is alarmed... ")
 print(r"""
@@ -63,8 +76,8 @@ print(r"""
 
 """)
 
-max_audio_value = int(val)
-print(" p to pause, give a banana to Scimia at paypal.me/Novecento99" + "\033[F" + "\033[F" + "\033[F")
+
+print(f" p to {colored('pause','red')}! give a banana to Scimia at  " + "\033[F" + "\033[F" + "\033[F")
 
 # Create the queue that will hold the audio chunks
 audio_queue = Queue()
@@ -88,7 +101,7 @@ def callback(indata: np.ndarray, outdata: np.ndarray, frames: int,
 
 
 with sd.Stream(callback=callback):
-    start=timer()
+    
     try:
         while True:
             # Pull a chunk of audio from the queue
@@ -100,7 +113,7 @@ with sd.Stream(callback=callback):
             if keyboard.is_pressed('p'):
                 # Flip the boolean
                 scimia_enabled = not scimia_enabled
-                print(f"\n\n p to {'pause' if scimia_enabled else 'resume'} " + "\033[F" * 3)
+                print(f"\n\n p to {colored('pause','red') if scimia_enabled else colored('resume','red')}!" + "\033[F" * 3)
                 time.sleep(0.3)
 
             # If we are quiet, then print the audio bar
@@ -110,10 +123,9 @@ with sd.Stream(callback=callback):
                 # Add the color red if we have passed the audio threshold
                 print(colored(' [' + '!' * BAR_LENGTH + ']', 'red'), end='\r')
                 # Play a sound to the user to let them know that they are loud
-                if scimia_enabled and (timer()-start)>10:
+                if scimia_enabled and (timer()-start)>4:
                     start = timer()
-                    print("lol")
-                    sd.play(data, fs)
+                    sd.play(data, samplerate)
                     sd.wait()
     except KeyboardInterrupt:
         print("\n\n\nStopping...")
