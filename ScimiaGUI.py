32acd6cc65ae88c3cf9be404678960e45d5817d4
Novecento <61213759+Novecento99@users.noreply.github.com>
2024-07-26 17:26:05 +0200

    bar to monitor mic

--- ScimiaGUI.py
+++ ScimiaGUI.py
@@ -19,11 +19,13 @@ from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import *
 from PyQt6 import QtGui
 from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import QTimer
 
 #         __o
 #       _ \<_
 #      (_)/(_)
 #```````         
+
 
 class scimiaWindow(QMainWindow):
     def __init__(self):
@@ -39,21 +41,41 @@ class scimiaWindow(QMainWindow):
         self.outputDevices =  [device for device in sd.query_devices() if (device["max_output_channels"] > 0)]
         self.listeningCheck = QCheckBox("enable listening")
         self.triggerCheck = QCheckBox("enable trigger")
-        self.thresHold = QLineEdit("100")
+        self.threshold = QLineEdit("100")
+        self.volumeBar = QProgressBar()
         self.inputSelector = QComboBox()
         self.inputSelector.addItems([device["name"] for device in self.inputDevices])
         self.outputSelector = QComboBox()
         self.outputSelector.addItems([device["name"] for device in self.outputDevices])
 
+        self.debugButton = QPushButton("debug")
+
         self.masterGrid.addWidget(self.listeningCheck)
         self.masterGrid.addWidget(self.triggerCheck)
-        self.masterGrid.addWidget(self.thresHold)
+        self.masterGrid.addWidget(self.threshold)
         self.masterGrid.addWidget(self.inputSelector)
         self.masterGrid.addWidget(self.outputSelector)
+        self.masterGrid.addWidget(self.volumeBar)
+        self.masterGrid.addWidget(self.debugButton)
+        self.volumeBar.setValue(1)
 
+        
+
+        def updateBar():
+            print("I have to update the bar")
+
+
+        timer = QTimer(self)
+        timer.timeout.connect(updateBar)
+        timer.start()
+        timer.setInterval(100)
+        print(timer.isActive())
+        print(timer.remainingTime())
+
+        self.debugButton.clicked.connect(updateBar)
         widget.setLayout(self.masterGrid)
         self.setCentralWidget(widget)
-        
+    
 
     
 if __name__ == '__main__':
