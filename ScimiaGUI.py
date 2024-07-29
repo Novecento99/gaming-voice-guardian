95ec71bd35d4c686cdd09f59d8dff63e6f74fc4a
Novecento <61213759+Novecento99@users.noreply.github.com>
2024-07-29 16:13:24 +0200

    removed useless depends

--- ScimiaGUI.py
+++ ScimiaGUI.py
@@ -1,28 +1,15 @@
 # generate sound instead of sound.wav?
 
-import pyaudio
-import os
 import sys
-import time
-import sys
-from queue import Queue
 
-import keyboard
 import numpy as np
 import sounddevice as sd
-import soundfile as sf
-#from termcolor import colored
-
-from timeit import default_timer as timer
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 from PyQt6.QtWidgets import QWidget
 from PyQt6.QtCore import QTimer
-
-import winsound
-frequency = 1000  # Set Frequency To 2500 Hertz
-duration = 100 # Set Duration To 1000 ms == 1 second
+from PyQt6.QtGui import QFont
 
 
 #         __o
@@ -30,31 +17,43 @@ duration = 100 # Set Duration To 1000 ms == 1 second
 #      (_)/(_)
 #```````         
 
-class scimiaWindow(QMainWindow):
+class monitorWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.widget = QWidget()
         
-        self.setWindowTitle("GamingListener")
+        self.setWindowTitle("voice monitor")
         self.setGeometry(500,120,200,120)
 
         self.masterGrid = QGridLayout()
         self.inputDevices =  [device for device in sd.query_devices() if (device["max_input_channels"] > 0)]
         self.outputDevices =  [device for device in sd.query_devices() if (device["max_output_channels"] > 0)]
-        self.listeningCheck = QCheckBox("enable listening")
+        #self.listeningCheck = QCheckBox("enable listening")
         self.triggerCheck = QCheckBox("enable trigger")
         self.multiplier = QLineEdit("100")
         self.volumeBar = QProgressBar()
         self.volumeBar.setOrientation(Qt.Orientation.Vertical)
         self.inputSelector = QComboBox()
-        self.inputSelector.addItems(set([device["name"].replace("\n", "")[0:80] for device in self.inputDevices]))
+        self.inputSelector.addItems(set([device["name"][0:80] for device in self.inputDevices]))
         self.outputSelector = QComboBox()
-        self.outputSelector.addItems(set([device["name"].replace("\n", "")[0:80] for device in self.outputDevices]))
+        self.outputSelector.addItems(set([device["name"][0:80] for device in self.outputDevices]))
         self.debugButton = QPushButton("debug")
         self.options = QCheckBox("show options")
 
+        self.volumeBar.setStyleSheet("""
+            QProgressBar {
+                border: 1px solid grey;
+                border-radius: 1px;
+                text-align: center;
+            }
 
-        self.masterGrid.addWidget(self.listeningCheck)
+            QProgressBar::chunk {
+                background-color: green;
+            }
+        """)
+        print(sd.default.device)
+
+        #self.masterGrid.addWidget(self.listeningCheck)
         self.masterGrid.addWidget(self.triggerCheck)
         self.masterGrid.addWidget(self.multiplier)
         self.masterGrid.addWidget(self.inputSelector)
@@ -68,9 +67,9 @@ class scimiaWindow(QMainWindow):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateProgressBar)
         self.timer.start()
-        self.timer.setInterval(10)
+        self.timer.setInterval(50)
 
-        self.debugButton.clicked.connect(self.playsound)
+        self.debugButton.clicked.connect(self.trigger)
         
         self.setCentralWidget(self.widget)
 
@@ -86,28 +85,39 @@ class scimiaWindow(QMainWindow):
         # Update the progress bar with the current volume level
         self.volumeBar.setValue(min(int(self.volume_level), 100))
         if (self.volume_level>100 and self.triggerCheck.isChecked()):
-            self.playsound()
+            self.trigger()
 
-    def playsound(self):
-        self.volumeBar.setStyleSheet("""
-            QProgressBar {
-                border: 2px solid grey;
-                border-radius: 5px;
-                text-align: center;
-            }
-
-            QProgressBar::chunk {
-                background-color: red;
-                width: 20px;
-            }
-        """)
-        winsound.Beep(frequency, duration)
-
+    def trigger(self):
+        self.play_tone()
     
+    def play_tone(self, freq=2500, duration=0.2, samplerate=44100, amplitude=0.5):
+        """
+        Play a sine wave tone.
+
+        Parameters:
+        - frequency: Frequency of the sine wave in Hz (default: 440 Hz, which is A4 note).
+        - duration: Duration of the tone in seconds (default: 1 second).
+        - samplerate: Sampling rate in samples per second (default: 44100 Hz).
+        - amplitude: Amplitude of the wave (default: 0.5, range: 0.0 to 1.0).
+        """
+        # Generate time points
+        t = np.linspace(0, duration, int(samplerate * duration), endpoint=False)
+
+        # Generate sine wave
+        wave = amplitude * np.sin(2 * np.pi * freq * t)
+
+        # Play the sound
+        sd.play(wave, samplerate)
+        sd.wait()  # Wait until the sound has finished playing
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    listenerWindow = scimiaWindow()
+    font = QFont("Aptos", 10)
+    app.setFont(font)
+    listenerWindow = monitorWindow()
     listenerWindow.show()
+    
     app.exec()
 
